a8f4ddb139ecd23a77b8f80d23ea33ad517126ba
Ayush Pathak <ayushpathak7777@gmail.com>
2024-12-22 22:37:47 +0530

    Add files via upload

---
+++ src/count_subarray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class count_subarray {
+
+    public static int findAllSubarraysWithGivenSum(int arr[], int k) {
+        int n = arr.length;
+        Map<Integer, Integer> mpp = new HashMap<>();
+        int preSum = 0; int cnt = 0;
+
+        mpp.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            preSum += arr[i];
+
+
+            int remove = preSum - k;
+
+            cnt += mpp.getOrDefault(remove,0);
+
+            mpp.put(preSum, mpp.getOrDefault(preSum,0) + 1);
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4};
+        int k = 6;
+        int cnt = findAllSubarraysWithGivenSum(arr, k);
+        System.out.println("The number of subarrays is: " + cnt);
+    }
+}
+
