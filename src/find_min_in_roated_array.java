b813249b85e01c4dd4c16fc2c29d7413edbfaf60
Ayush Pathak <ayushpathak7777@gmail.com>
2024-12-28 16:58:23 +0530

    Add files via upload

---
+++ src/find_min_in_roated_array.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class find_min_in_roated_array {
+    public static int findMin(int[] nums) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int ans=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[low]<=nums[mid]){
+                ans=Math.min(ans,nums[low]);
+                low=mid+1;
+            }
+            else{
+                ans=Math.min(ans,nums[mid]);
+                high=mid-1;
+            }    
+        }
+        return ans;
+    }
+     public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+         }
+         System.out.println(findMin(arr));
+    
+}
+}
