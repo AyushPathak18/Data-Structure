b98e39d2ec3e5e5250a99240053c81c808052662
Ayush Pathak <ayushpathak7777@gmail.com>
2024-12-28 22:06:09 +0530

    Add files via upload

---
+++ src/peak_element.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class peak_element {
+    public static int findPeakElement(int[] arr) {
+        int n = arr.length; //Size of array.
+
+
+    if (n == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if (arr[n - 1] > arr[n - 2]) return n - 1;
+
+    int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+     
+        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
+            return mid;
+
+    
+        if (arr[mid] > arr[mid - 1]) low = mid + 1;
+
+        
+        else high = mid - 1;
+    }
+    return -1;
+
+        
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+         }
+         System.out.println(findPeakElement(arr));
+    
+}
+}
